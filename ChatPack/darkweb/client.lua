396684cb3b9a524e9aca663e4efb1be7019eb5c5
Axxxmir <81544138+Axxxmir@users.noreply.github.com>
2021-11-10 08:21:10 -0600

    Delete ChatPack directory

--- ChatPack/darkweb/client.lua
+++
@@ -1,8 +0,0 @@
-
---Scripted by eknjack--
-
-RegisterCommand("darkweb", function(source, args, rawCommand)
-    local message = table.concat(args, " ")
-    TriggerClientEvent("chatMessage", -1, "Dark Web | Anonymous User ~", {255,0,0}, message)
-end)
-
--- ChatPack/twitter_script/tweet/__resource.lua
+++
@@ -1,2 +0,0 @@
-description 'Enables use of /tweet for in character chat.'
-server_script 'server.lua'
